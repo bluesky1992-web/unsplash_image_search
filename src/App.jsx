be27a0675107ff5215296a00bf201a5910790b71
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-10-15 22:07:19 +0300

    done !

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,66 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, useState } from 'react';
-import './index.css';
-import { Button, Form } from 'react-bootstrap';
 import axios from 'axios';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import './index.css';
+
 const API_URL = 'https://api.unsplash.com/search/photos';
 const IMAGES_PER_PAGE = 20;
-const App = () => {
-  const [images, setImages] = useState();
-  const [totalPages, setTotalPages] = useState();
-  const [page, setPage] = useState(1);
 
+const App = () => {
   const searchInput = useRef(null);
-  
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState();
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput.current.value) {
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`,
+        );
+        console.log('data', data);
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
+      setLoading(false);
+      console.log(error);
+    }
+  }, [page]);
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
   const resetSearch = () => {
-    setImages();
     setPage(1);
+    fetchImages();
   };
+
   const handleSearch = (event) => {
     event.preventDefault();
     console.log(searchInput.current.value);
     resetSearch();
   };
+
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
     resetSearch();
   };
+
   console.log('page', page);
-  const fetchImages = async () => {
-    try {
-      const { data } = await axios.get(
-        `${API_URL}?query=${
-          searchInput.current.value
-        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
-          import.meta.env.VITE_API_KEY
-        }`,
-      );
-      console.log('data', data);
-      setImages(data.results);
-      setTotalPages(data.total_pages);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchImages();
-  }, [page]);
+
   return (
     <div className="container">
-      <h1 className="title">image search</h1>
+      <h1 className="title">Image Search</h1>
+      {errorMsg && <p className="error-msg">{errorMsg}</p>}
       <div className="search-section">
         <Form onSubmit={handleSearch}>
           <Form.Control
@@ -64,24 +77,30 @@ const App = () => {
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
-      <div className="images">
-        {images?.map((image) => (
-          <img
-            key={image.id}
-            src={image.urls.small}
-            alt={image.alt_description}
-            className="image"
-          />
-        ))}
-      </div>
-      <div className="buttons">
-        {page > 1 && (
-          <Button onClick={() => setPage(page - 1)}>Previous</Button>
-        )}
-        {page < totalPages && (
-          <Button onClick={() => setPage(page + 1)}>Next</Button>
-        )}
-      </div>
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <>
+          <div className="images">
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className="image"
+              />
+            ))}
+          </div>
+          <div className="buttons">
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
